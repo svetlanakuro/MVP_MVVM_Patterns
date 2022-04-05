44f45f3d6c1494b75313c5284e30206cfc5cbf94
Svetlana Kuro <svetlana.kuro@gmail.com>
2022-04-06 02:09:59 +0300

    Initial commit

---
+++ app/src/main/java/com/svetlanakuro/mvp_mvvm_patterns/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.svetlanakuro.mvp_mvvm_patterns
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
